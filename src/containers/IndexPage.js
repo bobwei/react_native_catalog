00eb54b18c52dcd9dba49126bfc8f7aeb08f903e
bobwei <bob61016@gmail.com>
2016-05-05 16:10:50 +0800

    be able to start/stop recording location

--- src/containers/IndexPage.js
+++ src/containers/IndexPage.js
@@ -3,26 +3,40 @@
 import React from 'react';
 import {
   View,
-  MapView
+  MapView,
+  TouchableOpacity,
+  Text
 } from 'react-native';
+import { styles } from '../styles/IndexPage';
 
 
 export default class IndexPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isRecordingLocation: false,
       coordinates: []
     };
   }
 
-  onRegionChange(region) {
-    let { latitude, longitude } = region;
+  addCoordinateToOverlay(coordinate) {
     this.setState({
       coordinates: [
         ...this.state.coordinates,
-        { latitude, longitude }
+        coordinate
       ]
     });
+  }
+
+  onToggleRecordingLocation() {
+    this.setState({ isRecordingLocation: !this.state.isRecordingLocation });
+  }
+
+  onRegionChange(region) {
+    if (this.state.isRecordingLocation) {
+      let { latitude, longitude } = region;
+      this.addCoordinateToOverlay({ latitude, longitude });
+    }
   }
 
   render() {
@@ -31,6 +45,7 @@ export default class IndexPage extends React.Component {
         <MapView
           style={{ flex: 1 }}
           onRegionChange={this.onRegionChange.bind(this)}
+          showsUserLocation={true}
           region={{
             latitude: 24.9837193,
             longitude: 121.5427091,
@@ -43,6 +58,13 @@ export default class IndexPage extends React.Component {
             lineWidth: 9
           }]}
         />
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity onPress={this.onToggleRecordingLocation.bind(this)}>
+            <Text style={styles.buttonText}>
+              {(!this.state.isRecordingLocation)?'Start': 'Complete'}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
---
+++ src/styles/IndexPage.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  buttonWrapper: {
+    position: 'absolute',
+    right: 0,
+    bottom: 30,
+    left: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    padding: 20
+  }
+});
