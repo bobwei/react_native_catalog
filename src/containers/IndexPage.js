12950a87d3d30a538b475689f8eeccaf84732287
bobwei <bob61016@gmail.com>
2016-05-05 15:05:20 +0800

    be able to test map view

--- src/containers/IndexPage.js
+++ src/containers/IndexPage.js
@@ -2,14 +2,35 @@
 
 import React from 'react';
 import {
-  View
+  View,
+  MapView
 } from 'react-native';
 
 
 export default class IndexPage extends React.Component {
   render() {
     return (
-      <View />
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={{
+            latitude: 24.9837193,
+            longitude: 121.5427091,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+          }}
+          overlays={[{
+            coordinates:[
+              {latitude: 24.9787661, longitude: 121.5536013},
+              {latitude: 24.9887661, longitude: 121.5536013},
+              {latitude: 24.9987661, longitude: 121.5536013},
+              {latitude: 25.0087661, longitude: 121.5536013}
+            ],
+            strokeColor: '#f007',
+            lineWidth: 4
+          }]}
+        />
+      </View>
     );
   }
 }
